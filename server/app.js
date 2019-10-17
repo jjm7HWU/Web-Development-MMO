d29ef64d95682e1e2246da5fb5a3f32d347d8627
Jose Fernandes <jcc8@hw.ac.uk>
2019-10-17 10:10:42 +0100

    React installation and first deveopment configuration

--- server/app.js
+++ server/app.js
@@ -9,7 +9,8 @@ const app = express();
 
 
 app.get("/", (req, res) => {
-    res.sendFile( path.join(__dirname, "/../index.html") )
+    //res.sendFile( path.join(__dirname, "/../index.html") )
+    res.send("I am the server")
 })
 
 

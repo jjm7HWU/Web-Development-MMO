c1796fed31cf1bd64dd0da3d37e83df9a736a461
jjm7 <jjm7@hw.ac.uk>
2019-10-31 13:33:15 +0000

    added entities

--- public/js/client.js
+++ public/js/client.js
@@ -1,15 +1,36 @@
 // update all entities
 function update() {
-  // to be implemented
+  player.update();
+  for (const snake of snakes) snake.update();
 }
 
 // display all entities
 function display() {
-  // to be implemented
+  displayBackground();
+  player.display();
+  for (const snake of snakes) snake.display();
+  player.displayHead();
+  for (const snake of snakes) snake.displayHead();
 }
+
+document.addEventListener("keydown", event => {
+  switch (event.keyCode) {
+    case (87) : player.turn(0); break;
+    case (68) : player.turn(1); break;
+    case (83) : player.turn(2); break;
+    case (65) : player.turn(3); break;
+  }
+}
+);
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
+let player = new Player(0,0);
+let snakes = [];
+for (let c = 0; c < 100; c++) {
+  snakes.push(new Snake(random(-10,10), random(-10,10)));
+}
 
 setInterval(
   function() {

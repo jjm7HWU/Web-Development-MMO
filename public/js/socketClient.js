3579e49577a07098bfe508d5c709c4aad60be4e6
jcc8 <jcc8@hw.ac.uk>
2019-11-06 17:25:56 +0000

    adding comments

--- public/js/socketClient.js
+++ public/js/socketClient.js
@@ -1,35 +1,49 @@
 
-
+// initialise websocket
 var socket = io();
 
-
-
-
+// elements variables
 var form = document.getElementById("formInput")
 var input = document.getElementById("m");
 var messages = document.getElementById("messages")
 
 
-form.addEventListener("submit",function (e) {
-    e.preventDefault(); // prevents page reloading
+// form submit event handler
+form.addEventListener("submit",function (e) 
+{
+    // prevents page reloading
+    e.preventDefault();
+
+    // adding input value
     var val = input.value;
+
+    // resetting the input value
     input.value = "";
+
+    // emit socket event
     socket.emit("chat message", val)
 })
 
-
-
-
+// append list html element to unordered list
 var appendLi = function (message)
 {
+    // creates a new list html element
     var li = document.createElement("li");
+
+    // creates a new text html element
     var textNode = document.createTextNode(message);
+
+    // appends new text to list
     li.appendChild(textNode);
+
+    // appends list to ul html element
     messages.appendChild(li);
 }
 
-
-socket.on("chat message", function(msg) {
+// websocket event handler
+socket.on("chat message", function(msg) 
+{
+    // appends text to ul html element
     appendLi(msg);
 })
 

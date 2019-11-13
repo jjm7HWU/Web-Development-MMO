bfaebbb9489c2812230d84084fa3116382dad92e
jjm7 <jjm7@hw.ac.uk>
2019-11-13 17:09:22 +0000

    started sorting player initialization

--- public/js/socketClient.js
+++ public/js/socketClient.js
@@ -1,4 +1,4 @@
-function display(foods, snakes, arena) {
+function display(foods, player, snakes, arena) {
   // Display canvas
   displayBackground();
 
@@ -70,5 +70,6 @@ socket.on("game state", function(gameState)
   let foods = gameState.foodItems;
   let snakes = gameState.snakes;
   let arena = gameState.arena;
-  display(foods, snakes, arena);
+  let player = snakes[socket.id]; // TODO
+  display(foods, player, snakes, arena);
 })

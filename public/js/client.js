fbb5189abb628b18baa48cb7117827dd8fd98672
jjm7 <jjm7@hw.ac.uk>
2019-11-13 15:57:49 +0000

    refactored client side

--- public/js/client.js
+++ public/js/client.js
@@ -1,39 +1,16 @@
-function display() {
-  // Display canvas
-  displayBackground();
-
-  // Display all entities
-  for (const food of foods) food.display();
-  player.display();
-  for (const snake of snakes) snake.display();
-
-  // Display all entities' heads (temporary implementation)
-  player.displayHead();
-  for (const snake of snakes) snake.displayHead();
-
-  // TODO: Display interface
-}
-
 /* Event listener for handling key presses */
 document.addEventListener("keydown", event => {
+  var n;
   switch (event.keyCode) {
-    case (87) : player.turn(0); break; // W KEY to go up
-    case (68) : player.turn(1); break; // D KEY to go right
-    case (83) : player.turn(2); break; // S KEY to go down
-    case (65) : player.turn(3); break; // A KEY to go left
+    case (87) : n = 0; break; // W KEY to go up
+    case (68) : n = 1; break; // D KEY to go right
+    case (83) : n = 2; break; // S KEY to go down
+    case (65) : n = 3; break; // A KEY to go left
+    default : n = -1; break;
   }
+  if (n != -1) socket.emit("update dir", n);
 });
 
 // create canvas and get context
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
-let arena = new Arena(); // create arena
-let player = new Snake(0,0); // create player
-let {snakes, foods} = createEntities(); // create entities (TEMPORARY - to be done on server)
-
-/* Main game loop */
-setInterval(
-  function() {
-    display();
-}, 200); // call every 200 milliseconds

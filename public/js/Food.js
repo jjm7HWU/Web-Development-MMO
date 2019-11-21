be5b678288a967c3fcebc9e55a868f33208110c2
jjm7 <jjm7@hw.ac.uk>
2019-11-21 14:55:48 +0000

    created animation loop

--- public/js/Food.js
+++ public/js/Food.js
@@ -4,14 +4,13 @@ class Food {
     obj && Object.assign(this, obj);
   }
 
-  display() {
+  display(frameIndex) {
     // get plotting coordinates
     let X = getPlotX(this.x);
     let Y = getPlotY(this.y);
 
-    // draw food
-    foodAnimation.display(X, Y, this.frameIndex);
-    this.frameIndex++;
+    // draw food item at specified frame
+    foodAnimation.display(X, Y, frameIndex);
   }
 
 }
--- public/js/socketClient.js
+++ public/js/socketClient.js
@@ -1,4 +1,4 @@
-function display(foods, player, snakes, arena) {
+function display(frameCounter, foods, player, snakes, arena) {
   // Display canvas
   displayBackground();
 
@@ -8,7 +8,7 @@ function display(foods, player, snakes, arena) {
   // Cast and display all entities
   for (const food of foods) {
     _food = new Food(food);
-    if (0 <= _food.x <= player.x+X_PERIPHERAL && 0 <= _food.y <= player.y+Y_PERIPHERAL) _food.display();
+    if (0 <= _food.x <= player.x+X_PERIPHERAL && 0 <= _food.y <= player.y+Y_PERIPHERAL) _food.display(frameCounter);
   }
   _player.display();
   for (const snake of snakes) {
@@ -74,6 +74,8 @@ socket.on("chat message", function(msg)
     console.log("Server message: " + msg);
 })
 
+var foods;
+var snakes;
 var player;
 var arena;
 
@@ -81,14 +83,33 @@ var arena;
 socket.on("game state", function(gameState)
 {
   // retrieve entities from game state
-  let foods = gameState.foodItems;
-  let snakes = gameState.snakes;
+  foods = gameState.foodItems;
+  snakes = gameState.snakes;
   arena = gameState.arena;
+
   // identify player
   player = snakes.find(function(snake){
     return snake.id == socket.id;
   });
 
-  // display entities
-  display(foods, player, snakes, arena);
+  /* local loop for showing animated frames between server game loop */
+
+  // time loop was last called (zero to call immediately)
+  let lastTime = 0;
+  // current animation frame
+  let frameCounter = 0;
+
+  const callback = (millis) => {
+    if (millis-lastTime > 33) {                             // every 33ms
+      display(frameCounter, foods, player, snakes, arena);  // display entities
+      frameCounter++;                                       // count animation frame
+      lastTime = millis;                                    // record time at end of this loop
+    }
+    if (frameCounter < 6) {                                 // if not yet last frame
+      window.requestAnimationFrame(callback);               // call next animation frame
+    }
+  };
+
+  // call initial loop
+  callback();
 })
